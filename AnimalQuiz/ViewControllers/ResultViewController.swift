7857fd1ec1bf8a41f9013a1193d98097a78f9ed0
pptichka <taleskina@gmail.com>
2022-10-11 14:38:36 +0300

    all completed

--- AnimalQuiz/ViewControllers/ResultViewController.swift
+++ AnimalQuiz/ViewControllers/ResultViewController.swift
@@ -18,10 +18,33 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         
         navigationItem.hidesBackButton = true
+        updateResult()
     }
     
     @IBAction func donaButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
     }
 
+}
+
+extension ResultViewController {
+    private func updateResult() {
+        var frequencyOfAnimals: [Animal : Int] = [:]
+        let animals = userAnswers.map { $0.animal }
+        
+        for animal in animals {
+            frequencyOfAnimals[animal, default: 0] += 1
+        }
+        
+        let sortedFrequencyOfAnimals = frequencyOfAnimals.sorted { $0.value > $1.value
+        }
+        guard let mostFrequencyAnimal = sortedFrequencyOfAnimals.first?.key else { return }
+        
+        updateUI(with: mostFrequencyAnimal)
+    }
+    
+    private func updateUI(with animal: Animal) {
+        animalTypeLabel.text = "Вы - \(animal.rawValue)!"
+        descriptionLabel.text = animal.definition
+    }
 }
